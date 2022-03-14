e6d99317049c02f9801da5e78e0abb851aa2fcfb
Jay Klehr <jay@diablomedia.com>
2022-03-14 11:13:56 -0600

    updating deps and workflow for PHP 8.1

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,13 +2,16 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->exclude('vendor')
+    ->files()
     ->in(__DIR__)
 ;
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+return $config->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
     ])
+    ->setUsingCache(true)
     ->setFinder($finder)
 ;
